73e890ee45e92830b7fc5246b64962a0fa85c7eb
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-04-25 23:39:06 +0000

    Initial import

---
+++ common.py
@@ -0,0 +1,32 @@
+# Neighborhood Weather Net
+# Copyright (C) 2003 Iowa State University
+# Written by Travis B. Hartwell
+# 
+# This module is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+""" Neighborhood Weather Net Common Definitions
+"""
+
+# Prompts and messages and their lengths
+loginPrompt = "login> "
+loginPromptLen = len(loginPrompt)
+
+passwordPrompt = "password> "
+passwordPromptLen = len(passwordPrompt)
+
+promptEnding = "> "
+promptEndingLen = len(promptEnding)
+
+successMessage = "***Login successful"
+successMessageLen = len(successMessage)
